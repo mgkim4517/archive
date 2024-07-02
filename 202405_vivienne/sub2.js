8c1ed8c26ef6203a929e16878d9178526e7681ad
mgkim4517 <ooowaaa@naver.com>
2024-07-02 11:10:11 +0900

    240702

--- 202405_vivienne/sub2.js
+++
@@ -1,8 +0,0 @@
-$('.info-toggle').click(function() {
-  if ($(this).hasClass('open')) {
-      $(this).removeClass('open').next('.toggle-contents').slideUp();
-  } else {
-      $('.info-toggle').removeClass('open').next('.toggle-contents').slideUp();
-      $(this).addClass('open').next('.toggle-contents').slideDown();
-  }
-});
